0b78c91ec2607ba9519c01f6a4bde0f467a1f106
Itsuro Oda <oda@valinux.co.jp>
2015-02-09 08:11:43 +0900

    change oslo module name

--- metaplugin/plugin/meta_neutron_plugin.py
+++ metaplugin/plugin/meta_neutron_plugin.py
@@ -13,8 +13,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from oslo.config import cfg
-from oslo.utils import importutils
+from oslo_config import cfg
+from oslo_utils import importutils
 
 from metaplugin.plugin import meta_db_v2
 from neutron.common import exceptions as exc
